c7fa54742cdd9b85e2a5aca7f5c5df5aec8913fa
Joshua Hyde <jrh3k5@gmail.com>
2025-02-21 07:11:36 -0600

    Rename CounterScript

--- script/Ploan.s.sol
+++ script/Ploan.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.26;
 import {Script, console} from "forge-std/Script.sol";
 import {Ploan} from "../src/Ploan.sol";
 
-contract CounterScript is Script {
+contract PloanScript is Script {
     Ploan public ploan;
 
     function setUp() public {}
