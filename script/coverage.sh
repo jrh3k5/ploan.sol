536ae34f1dc8951ef27a3f4a1168a0a3f822390e
Joshua Hyde <jrh3k5@gmail.com>
2025-02-23 08:35:59 -0600

    Disable initializers in constructor (#22)
    * Disable initializers in constructor
    
    * Add coverage reporting
    
    * Fix coverage minimum

---
+++ script/coverage.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+MINIMUM_COV=90
+COV_PERCENTAGE=$(forge coverage | grep src/Ploan.sol | tr "|" "\n" | grep "%" | head -n 1 | awk -F '%' '{print $1}')
+if [ $(bc <<< "$COV_PERCENTAGE < $MINIMUM_COV") -eq 1 ]; then
+    echo "$COV_PERCENTAGE is below minimum coverage threshold ($MINIMUM_COV)";
+    exit 1
+else
+    echo "$COV_PERCENTAGE is above minimum coverage threshold ($MINIMUM_COV)";
+fi
