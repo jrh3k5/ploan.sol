2d734a313b2c4ae72f491e682365a10452868de1
Joshua Hyde <jrh3k5@gmail.com>
2025-02-28 20:59:57 -0600

    Attempt to upgrade existing contract

--- script/UpgradesScript.s.sol
+++ script/UpgradesScript.s.sol
@@ -16,10 +16,19 @@ contract UpgradesScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        // Deploy `Ploan` as a transparent proxy using the Upgrades Plugin
-        address transparentProxy =
+        address transparentProxy = vm.envOr("TRANSPARENT_PROXY_ADDRESS", address(0));
+        if (transparentProxy == address(0)) {
+            console.log("Deploying a new instance of the contract");
+
             Upgrades.deployTransparentProxy("Ploan.sol", msg.sender, abi.encodeCall(Ploan.initialize, ()));
 
-        console.log("Deployed contract to address", transparentProxy);
+            console.log("Deployed contract to address", transparentProxy);
+
+            return;
+        }
+
+        console.log("Upgrading contract at address", transparentProxy);
+
+        Upgrades.upgradeProxy(transparentProxy, "Ploan.sol", "");
     }
 }
